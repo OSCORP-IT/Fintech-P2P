045e593b395098edfcad42a95c88ee846b4f8ef1
Shubhra-dev <shuuvro.dev@gmail.com>
2024-01-02 02:04:41 +0600

    Educational Qualification & Outstanding Loan input field adding funtionality added

--- src/features/user/Education.jsx
+++ src/features/user/Education.jsx
@@ -1,4 +1,19 @@
+import { useState } from "react";
+
 function Education() {
+  const [inputSets, setInputSets] = useState([
+    { exam: "", institute: "", year: "" },
+  ]);
+
+  const addInputSet = () => {
+    setInputSets([...inputSets, { exam: "", institute: "", year: "" }]);
+  };
+
+  const handleChange = (index, field, value) => {
+    const updatedInputSets = [...inputSets];
+    updatedInputSets[index][field] = value;
+    setInputSets(updatedInputSets);
+  };
   return (
     <div>
       <h2 className="text-base font-semibold py-2">
@@ -15,41 +30,53 @@ function Education() {
           </div>
           <div className="rounded-md py-2 bg-gray-200 w-1/6">Passing Year</div>
         </div>
-        <div className="flex items-center gap-2 text-center text-base mt-1">
-          <div className="rounded-md py-1 flex-grow w-2/6">
-            <select
-              id="exam"
-              required
-              className="w-full rounded-md px-4 py-3 border border-gray-300"
-            >
-              <option value="">--Select--</option>
-              <option value="ssc">SSC</option>
-              <option value="hsc">HSC</option>
-              <option value="bachelor">Bachelor</option>
-              <option value="masters">Masters</option>
-            </select>
-          </div>
-          <div className="rounded-md py-1 flex-grow w-3/6">
-            <input
-              type="text"
-              name="institute_name"
-              id="institute_name"
-              placeholder="Institute Name"
-              className="text-base text-center font-normal rounded-lg w-full border border-gray-300 py-3"
-            />
-          </div>
-          <div className="rounded-md py-1 w-1/6">
-            <input
-              type="text"
-              name="passing_year"
-              id="passing_year"
-              placeholder="Passing Year"
-              className="text-base text-center font-normal rounded-lg w-full border border-gray-300 py-3"
-            />
-          </div>
-        </div>
+        {inputSets.map((inputSet, index) => (
+          <>
+            <div className="flex items-center gap-2 text-center text-base mt-1">
+              <div className="rounded-md py-1 flex-grow w-2/6">
+                <select
+                  id="exam"
+                  onChange={(e) => handleChange(index, "exam", e.target.value)}
+                  required
+                  className="w-full rounded-md px-4 py-3 border border-gray-300"
+                >
+                  <option value="">--Select--</option>
+                  <option value="ssc">SSC</option>
+                  <option value="hsc">HSC</option>
+                  <option value="bachelor">Bachelor</option>
+                  <option value="masters">Masters</option>
+                </select>
+              </div>
+              <div className="rounded-md py-1 flex-grow w-3/6">
+                <input
+                  type="text"
+                  name="institute_name"
+                  onChange={(e) =>
+                    handleChange(index, "institute", e.target.value)
+                  }
+                  id="institute_name"
+                  placeholder="Institute Name"
+                  className="text-base text-center font-normal rounded-lg w-full border border-gray-300 py-3"
+                />
+              </div>
+              <div className="rounded-md py-1 w-1/6">
+                <input
+                  type="text"
+                  name="passing_year"
+                  id="passing_year"
+                  onChange={(e) => handleChange(index, "year", e.target.value)}
+                  placeholder="Passing Year"
+                  className="text-base text-center font-normal rounded-lg w-full border border-gray-300 py-3"
+                />
+              </div>
+            </div>
+          </>
+        ))}
         <div className="py-2">
-          <h2 className="text-base font-medium py-1 rounded-md cursor-pointer bg-emeraldGreen text-white text-center">
+          <h2
+            onClick={addInputSet}
+            className="text-base font-medium py-1 rounded-md cursor-pointer bg-emeraldGreen text-white text-center"
+          >
             Add Education +
           </h2>
         </div>

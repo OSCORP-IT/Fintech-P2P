a53ad3099469757e16ea8bc989d9d656c370b733
Shubhra-dev <shuuvro.dev@gmail.com>
2024-02-20 22:32:58 +0600

    Revised design updated - progress bar on loan details

--- src/features/pages/about/AboutUs.jsx
+++ src/features/pages/about/AboutUs.jsx
@@ -3,21 +3,12 @@ import TrustyBoard from "./TrustyBoard";
 import NewsLetter from "../../../ui/Newsletter";
 import WhyUs from "./WhyUs";
 function AboutUs() {
-  const handlePrint = () => {
-    window.print();
-  };
   return (
     <>
       <AboutHero />
       <WhyUs />
       <TrustyBoard />
       <NewsLetter />
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={handlePrint}
-      >
-        Print
-      </button>
     </>
   );
 }
